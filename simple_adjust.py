12c8da3edadd0060ceaa900d0b0ab482fe5e742c
lukegregorio <lukegregorio99@gmail.com>
2022-11-27 14:40:31 +0000

    document simple_adjust.py

--- simple_adjust.py
+++ simple_adjust.py
@@ -1,14 +1,24 @@
+"""
+Adjust the subtitle speed assuming that the end of the subtitles 'match' the end of the video file
+"""
+
 import pysrt
 from moviepy.editor import VideoFileClip
 from datetime import datetime
 
 def get_video_duration(filename):
+    """
+    Get video duration in seconds
+    """
     clip = VideoFileClip(filename)
     duration = clip.duration
 
     return duration
 
 def get_subs_duration(subs):
+    """
+    Get subtitles duration in seconds
+    """
     t = subs[-1].end.to_time()
     
     # convert to seconds
@@ -18,18 +28,18 @@ def get_subs_duration(subs):
 
 
 if __name__ == "__main__":
+
     srt_file_path = input("Enter the path of the subtitles file: ")
     video_file_path = input("Enter the path of the video file: ")
 
     subs = pysrt.open(srt_file_path)
 
-    subs_duration = get_subs_duration(srt_file_path)
+    subs_duration = get_subs_duration(subs)
     video_duration = get_video_duration(video_file_path)
-
-    ratio = video_duration / subs_duration
+    
+    # get the ratio between video and subtitles duration which is used to adjust the subtitles timings - assumes the 'endings' of both files are the same
+    ratio = subs_duration / video_duration
 
     subs.shift(ratio = ratio)
 
-    subs.save('adjusted.srt', encoding='utf-8')
-
-# TODO : test and document
+    subs.save('output/adjusted.srt', encoding='utf-8')
